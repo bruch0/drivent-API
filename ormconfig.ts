f06285c7bd4267145f6097a52a699c18b91aa594
bruch0 <lucas.bruch0@gmail.com>
2022-01-19 14:53:13 -0300

    fix: add heroku required configs

--- ormconfig.ts
+++ ormconfig.ts
@@ -8,8 +8,13 @@ export default {
   migrationsTableName: "migrations",
   entities: ["dist/entities/*.js"],
   migrations: ["dist/migrations/*.js"],
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
   cli: {
     migrationsDir: "src/migrations",
-    entitiesDir: "dist/entities/*.js"
-  }
+    entitiesDir: "dist/entities/*.js",
+  },
 };
