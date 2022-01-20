6877a737eb445cee4071b2c49fc54f7d96aa6781
Bruno Balbuena <bbalbuena@pop-os.localdomain>
2022-01-20 11:59:25 -0300

    feat: create Hotel and Booking entities

---
+++ src/entities/Hotel.ts
@@ -0,0 +1,23 @@
+import {
+  BaseEntity,
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+} from "typeorm";
+import Booking from "./Booking";
+
+@Entity("hotels")
+export default class Hotel extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  roomTypes: string;
+
+  @OneToMany(() => Booking, booking => booking.hotel)
+  bookings: Booking[];
+}
