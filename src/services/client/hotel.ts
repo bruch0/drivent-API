c90f26b2b3fda1875fab5a919868ae2844389333
fernandollisboa <fernando.costa@ccc.ufcg.edu.br>
2022-01-21 10:16:22 -0300

    chore: alter migration:run script

--- src/services/client/hotel.ts
+++ src/services/client/hotel.ts
@@ -1,9 +1,8 @@
-import Enrollment from "@/entities/Enrollment";
-import Hotel from "@/entities/Hotel";
-import Payment from "@/entities/Payment";
+import Enrollment from '@/entities/Enrollment';
+import Hotel from '@/entities/Hotel';
 
 export async function getHotelsInfo(userId: number) {
-  const enrollment =  await Enrollment.getByUserIdWithAddress(userId);
-  return enrollment
-  //TO-DO tem que ver se tem um hotel registrado no nome da pesssoa
+	const enrollment = await Enrollment.getByUserIdWithAddress(userId);
+	return enrollment;
+	//TO-DO tem que ver se tem um hotel registrado no nome da pesssoa
 }
