4973b80f85a56d8e195e7303dc3ea95dafbcc4f4
Bruno Balbuena <bbalbuena@pop-os.localdomain>
2022-01-22 04:48:22 -0300

    feat: change getRooms query to return room availability as int instead of string

--- src/entities/Hotel.ts
+++ src/entities/Hotel.ts
@@ -25,11 +25,11 @@ export default class Hotel extends BaseEntity {
     const result = await this.createQueryBuilder("hotel")
       .select("booking.roomNumber", "roomNumber")
       .addSelect(
-        "SUM(CASE WHEN booking.isTaken = FALSE THEN 1 ELSE 0 END)",
+        "SUM(CASE WHEN booking.isTaken = FALSE THEN 1 ELSE 0 END)::INTEGER",
         "available"
       )
       .addSelect(
-        "SUM(CASE WHEN booking.isTaken = TRUE THEN 1 ELSE 0 END)",
+        "SUM(CASE WHEN booking.isTaken = TRUE THEN 1 ELSE 0 END)::INTEGER",
         "unavailable"
       )
       .leftJoin("hotel.bookings", "booking")
