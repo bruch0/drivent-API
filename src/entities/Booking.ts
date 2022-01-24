0b865c0a80e7af4e9388e232c9babc371bed900c
rabbithay <thay.csb@gmail.com>
2022-01-23 21:34:13 -0300

    feat: add booking methods to entity

--- src/entities/Booking.ts
+++ src/entities/Booking.ts
@@ -6,6 +6,7 @@ import {
   OneToOne,
   JoinColumn,
   ManyToOne,
+  getConnection,
 } from "typeorm";
 import Hotel from "./Hotel";
 import User from "./User";
@@ -27,4 +28,39 @@ export default class Booking extends BaseEntity {
   @OneToOne(() => User)
   @JoinColumn()
   user: User;
+
+  static async bookTheRoom(bookingId: number, userId: number) {    
+    await this.relateBookingToUser(bookingId, userId);
+    return await this.setBookingStatus(bookingId, true);
+  }
+
+  static async freeTheRoom(bookingId: number) {
+    await this.relateBookingToUser(bookingId, null);
+    return await this.setBookingStatus(bookingId, false);
+  }
+
+  static async getReservationDetails(bookingId: number) {
+    return await this.findOne(bookingId);
+  }
+
+  static async setBookingStatus(bookingId: number, isTaken: boolean) {
+    const results = await getConnection()
+      .createQueryBuilder()
+      .update(Booking)
+      .set({
+        isTaken        
+      })
+      .where("id = :id", { id: bookingId })
+      .execute();
+  
+    return results;
+  }
+
+  static async relateBookingToUser(bookingId: number, userId: number) {
+    await getConnection()
+      .createQueryBuilder()
+      .relation(Booking, "user")
+      .of(bookingId)
+      .set(userId);
+  }
 }
