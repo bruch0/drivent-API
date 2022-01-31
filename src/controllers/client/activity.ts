d90d8097d03cfa6da79503b12afdccaecb8a8627
fernandollisboa <fernando.costa@ccc.ufcg.edu.br>
2022-01-31 15:15:18 -0300

    fix: activity bug

--- src/controllers/client/activity.ts
+++ src/controllers/client/activity.ts
@@ -15,12 +15,12 @@ export async function getActivitiesDateInfo(req: Request, res: Response) {
 }
 
 export async function postNewActivity(req: Request, res: Response) {
-  const  userId  = req.user.id;
+  const userId = req.user.id;
   const { activityId } = req.body;
 
   const [activity] = await activitiesService.getActivityById(activityId);
   const { duration, time } = activity;
-  
+
   const userActivities = await activitiesService.getUserActivities(userId);
   const timeConflict = userActivities.find((activity) => {
     //se o dia for igual e a hora for igual
@@ -28,7 +28,11 @@ export async function postNewActivity(req: Request, res: Response) {
     //se a atividade a ser cadastrada durar mais de uma hora e a atividade já cadastrada começar em uma dessas horas seguintes
     if (duration > 1 && dayjs(activity.time) === dayjs(time).add(duration - 1, "hour")) return true;
     //se a atividade já cadastrada durar mais de uma hora e a atividade a ser cadastrada começar em uma dessas horas seguintes
-    if (activity.duration > 1 && dayjs(activity.time) === dayjs(time).subtract(activity.duration - 1, "hour")) return true;
+    if (
+      activity.duration > 1 &&
+      dayjs(activity.time) === dayjs(time).subtract(activity.duration - 1, "hour")
+    )
+      return true;
   });
 
   if (timeConflict) return res.sendStatus(409);
@@ -49,6 +53,5 @@ export async function getActivitiesByDay(req: Request, res: Response) {
     return res.sendStatus(httpStatus.NO_CONTENT);
   }
 
-  res.send(activities).status(httpStatus.OK);
+  res.status(httpStatus.OK).send(activities);
 }
-
